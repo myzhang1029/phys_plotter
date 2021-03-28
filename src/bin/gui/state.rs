fa0377a397adf97695c158ac55c84e3eea175fe3
Zhang Maiyun <myzhang1029@hotmail.com>
2021-03-28 10:49:21 +0800

    Fix a few save issues
    Signed-off-by: Zhang Maiyun <myzhang1029@hotmail.com>

--- src/bin/gui/state.rs
+++ src/bin/gui/state.rs
@@ -151,31 +151,31 @@ impl TryInto<PhysPlotterFile> for UIState {
         Ok(PhysPlotterFile {
             creator: defv::APP_ID.to_string(),
             version: crate_version!().to_string(),
+            title: self.title.get_text(),
             backend_name: format!("{}", self.backend),
             x_label: self.x_label.get_text(),
             y_label: self.y_label.get_text(),
             default_x_uncertainty: self.default_x_uncertainty.get_text().parse()?,
             default_y_uncertainty: self.default_x_uncertainty.get_text().parse()?,
             dataset: self.dataset_str(),
-            title: self.file_path,
         })
     }
 }
 
 /// Load saved file
 impl TryFrom<PhysPlotterFile> for UIState {
-    type Error = std::io::Error;
+    type Error = BackendsFromStrError;
     fn try_from(that: PhysPlotterFile) -> Result<Self, Self::Error> {
         Ok(Self {
             saved: true,
             file_path: Default::default(),
-            title: EntryBuffer::new(Some(defv::TITLE)),
+            title: EntryBuffer::new(Some(&that.title)),
             dataset: TextBufferBuilder::new().text(&that.dataset).build(),
-            backend: Backends::from_str(defv::BACKEND).unwrap(),
-            x_label: EntryBuffer::new(Some(defv::X_LABEL)),
-            y_label: EntryBuffer::new(Some(defv::Y_LABEL)),
-            default_x_uncertainty: EntryBuffer::new(Some(defv::X_UNCERTAINTY)),
-            default_y_uncertainty: EntryBuffer::new(Some(defv::X_UNCERTAINTY)),
+            backend: Backends::from_str(&that.backend_name)?,
+            x_label: EntryBuffer::new(Some(&that.x_label)),
+            y_label: EntryBuffer::new(Some(&that.y_label)),
+            default_x_uncertainty: EntryBuffer::new(Some(&that.default_x_uncertainty.to_string())),
+            default_y_uncertainty: EntryBuffer::new(Some(&that.default_y_uncertainty.to_string())),
         })
     }
 }
